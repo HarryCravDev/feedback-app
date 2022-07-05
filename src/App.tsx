6475a098074e8d4a30cd863e2d0f7d60da0cd50a
Harry Craven <harrycraven.dev@gmail.com>
2022-07-05 13:56:16 +0100

    Adding components to app file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import FeedbackItem from "./components/feedback-item/FeedbackItem";
+import Header from "./components/Header/Header";
+import { FeedbackProvider } from "./context/FeedbackContext";
 import Feedback from "./pages/Feedback";
 function App() {
 	return (
-		<div>
-			<Feedback />
-		</div>
+		<FeedbackProvider>
+			<div style={{ minHeight: "100vh", backgroundColor: "ghostwhite" }}>
+				<Header text="Feedback UI" />
+				<div style={{ paddingLeft: "2rem", paddingRight: "2rem" }}>
+					<Feedback />
+				</div>
+			</div>
+		</FeedbackProvider>
 	);
 }
 
