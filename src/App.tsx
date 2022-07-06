9321456daa6bd625681450261a0fd08e5b14e6fc
Harry Craven <harrycraven.dev@gmail.com>
2022-07-06 09:19:18 +0100

    feature/adding-feedback-form

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import FeedbackStats from "./components/feedback-stats/FeedbackStats";
 import Header from "./components/Header/Header";
 import { FeedbackProvider } from "./context/FeedbackContext";
 import Feedback from "./pages/Feedback";
+import FeedbackForm from "./components/feedback-form/FeedbackForm";
 function App() {
 	return (
 		<FeedbackProvider>
 			<div style={{ minHeight: "100vh", backgroundColor: "ghostwhite" }}>
-				<Header text="Feedback UI" />
+				<div style={{ marginBottom: "1rem" }}>
+					<Header text="Feedback UI" />
+				</div>
 				<div style={{ paddingLeft: "2rem", paddingRight: "2rem" }}>
+					<FeedbackForm />
 					<FeedbackStats />
 					<Feedback />
 				</div>
